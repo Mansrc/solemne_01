f62d02996671705c5cb4fa499fc54904a82e99f1
LuisGiadach <luisgiadachpino@gmail.com>
2021-09-29 20:40:31 -0300

    ALGUNAWEA

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import Registrarse from "./pages/Registrarse"
 import Acerca_Nosotros from "./pages/Acerca_nosotros"
 import ReservarHora1 from "./pages/ReservarHora"
 import ReservarHora2 from "./pages/ReservarHora2"
+import Calendario from './components/Calendario';
 
 function App() {
   return (
@@ -59,6 +60,9 @@ function App() {
         <Route exact path="/servicios/scannerGold/vehiculo">
             <ReservarHora2/>
         </Route>
+        <Route exact path="/servicios/scannerGold/vehiculo/algo">
+            <Calendario/>
+            </Route>
         
       </Switch>
       
---
+++ src/components/Calendario.jsx
@@ -0,0 +1,14 @@
+import Calendar from 'react-calendar'
+import Navbar from './Navbar'
+import Separacion from './Separacion'
+import 'react-calendar/dist/Calendar.css'
+function Calendario(){
+	return(
+<div>
+<Navbar/>
+<Separacion/>
+<Calendar/>
+</div>)
+}
+
+export default Calendario 
